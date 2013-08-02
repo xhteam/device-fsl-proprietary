063e1763fc08560a3d69a524aff2ce2119b9aaa6
raymond.wang <raymond1860@gmail.com>
2013-08-02 13:23:30 +0800

    unify nfc.xxx.so hardware module name to nfc.freescale.so

--- nfc/Android.mk
+++ nfc/Android.mk
@@ -15,7 +15,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := nfc.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := nfc.freescale
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := nfc_hw.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
